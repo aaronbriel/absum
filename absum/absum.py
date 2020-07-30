6a24fae42cde7e1b0baef0f9e2c2c344b56d6f43
Aaron Briel <aaronbriel@gmail.com>
2020-07-30 15:23:57 -0500

    added full AbSumAugmentor class documentation

--- absum/absum.py
+++ absum/absum.py
@@ -1,6 +1,18 @@
-import argparse
+# coding=utf-8
+# Copyright 2020 Aaron Briel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from box import Box
 from multiprocessing import Process
 import numpy as np
 import pandas as pd
@@ -9,14 +21,90 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 
 class AbSumAugmentor(object):
-    def __init__(self, args):
-        self.args = args
-        self.features = args.features
+    """
+    Uses Abstract Summarization for Data Augmentation to address multi-label class imbalance.
+    Parameters:
+        df (:class:`pandas.Dataframe`): Dataframe containing text and one-hot encoded features.
+        text (:obj:`string`, `optional`, defaults to "text"): Column in df containing text.
+        features (:obj:`list`, `optional`, defaults to None): Features to possibly augment data for.
+        device (:class:`torch.device`, `optional`, defaults to 'cuda' if available otherwise 'cpu'):
+            Torch device to run on cuda if available otherwise cpu.
+        model (:class:`~transformers.T5ForConditionalGeneration`, `optional`, defaults to 't5-small'):
+            T5ForConditionalGeneration model from pretrained 't5-small'.
+        tokenizer (:class:`~transformers.T5Tokenizer`, `optional`, defaults to 't5-small'):
+            T5Tokenizer model from pretrained 't5-small'.
+        return_tensors (:obj:str, `optional`, defaults to "pt") – Can be set to ‘tf’, ‘pt’ or ‘np’ to return
+            respectively TensorFlow tf.constant, PyTorch torch.Tensor or Numpy :oj: np.ndarray instead of a
+            list of python integers.
+        num_beams (:obj:`int`, `optional`, defaults to 4): Number of beams for beam search. Must be between 1
+            and infinity. 1 means no beam search. Default to 1.
+        no_repeat_ngram_size (:obj:`int`, `optional`, defaults to 4): If set to int > 0, all ngrams of size
+            no_repeat_ngram_size can only occur once.
+        min_length (:obj:`int`, `optional`, defaults to 10): The min length of the sequence to be generated.
+            Between 0 and infinity. Default to 10.
+        max_length (:obj:`int`, `optional`, defaults to 50): The max length of the sequence to be generated.
+            Between min_length and infinity. Default to 50.
+        early_stopping (:obj:`bool`, `optional`, defaults to True): bool if set to True beam search is stopped
+            when at least num_beams sentences finished per batch. Defaults to False as defined in
+            configuration_utils.PretrainedConfig.
+        skip_special_tokens (:obj:`bool`, `optional`, defaults to True): Don't decode special tokens
+            (self.all_special_tokens). Default: False.
+        num_samples (:obj:`int`, `optional`, defaults to 100): Number of samples to pull from dataframe with
+            specific feature to use in generating new sample with Abstract Summarization.
+        threshold (:obj:`int`, `optional`, defaults to 3500): Maximum ceiling for each feature, normally the
+            under-sample max.
+        multiproc (:obj:`bool`, `optional`, defaults to True): If set, stores calls for Abstract Summarization in
+            array which is then passed to run_cpu_tasks_in_parallel to allow for increasing performance through
+            multiprocessing.
+        debug (:obj:`bool`, `optional`, defaults to True): If set, prints generated summarizations.
+    """
+    def __init__(
+            self,
+            df,
+            text='text',
+            features=None,
+            device=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
+            model=T5ForConditionalGeneration.from_pretrained('t5-small'),
+            tokenizer=T5Tokenizer.from_pretrained('t5-small'),
+            return_tensors='pt',
+            num_beams=4,
+            no_repeat_ngram_size=4,
+            min_length=10,
+            max_length=50,
+            early_stopping=True,
+            skip_special_tokens=True,
+            num_samples=100,
+            threshold=3500,
+            multiproc=True,
+            debug=True
+    ):
+        self.df = df
+        self.text = text
+        self.features = features
+        self.device = device
+        self.model = model
+        self.tokenizer = tokenizer
+        self.return_tensors = return_tensors
+        self.num_beams = num_beams
+        self.no_repeat_ngram_size = no_repeat_ngram_size
+        self.min_length = min_length
+        self.max_length = max_length
+        self.early_stopping = early_stopping
+        self.skip_special_tokens = skip_special_tokens
+        self.num_samples = num_samples
+        self.threshold = threshold
+        self.multiproc = multiproc
+        self.debug = debug
         self.append_index = 0
-        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        self.model = T5ForConditionalGeneration.from_pretrained(args.model)
-        self.tokenizer = T5Tokenizer.from_pretrained(args.model)
         self.df_append = None
+
+        # If features passed in, convert to list of strings. Otherwise assume all features
+        # aside from text are in play
+        if self.features:
+            self.features = self.features.split(",")
+        else:
+            self.features = self.df.columns.tolist()
+            self.features.remove(self.text)
 
     def get_abstract_summary(self, text):
         """
@@ -30,28 +118,28 @@ class AbSumAugmentor(object):
         if self.device.type == 'cpu':
             tokenized_text = self.tokenizer.encode(
                 t5_prepared_text,
-                return_tensors=self.args.return_tensors).to(self.device)
+                return_tensors=self.return_tensors).to(self.device)
         else:
             tokenized_text = self.tokenizer.encode(
                 t5_prepared_text,
-                return_tensors=self.args.return_tensors)
+                return_tensors=self.return_tensors)
 
         summary_ids = self.model.generate(tokenized_text,
-                                          num_beams=self.args.num_beams,
-                                          no_repeat_ngram_size=self.args.no_repeat_ngram_size,
-                                          min_length=self.args.min_length,
-                                          max_length=self.args.max_length,
-                                          early_stopping=self.args.early_stopping)
+                                          num_beams=self.num_beams,
+                                          no_repeat_ngram_size=self.no_repeat_ngram_size,
+                                          min_length=self.min_length,
+                                          max_length=self.max_length,
+                                          early_stopping=self.early_stopping)
 
         output = self.tokenizer.decode(summary_ids[0],
-                                       skip_special_tokens=self.args.skip_special_tokens)
+                                       skip_special_tokens=self.skip_special_tokens)
 
-        if self.args.debug:
-            print("\n\nSummarized text: \n", output)
+        if self.debug:
+            print("\nSummarized text: \n", output)
 
         return output
 
-    def abs_sum_augment(self, df, num_samples=100, multiproc=True):
+    def abs_sum_augment(self):
         """
         Gets append counts (number of rows to append) for each feature and initializes main
         classes' dataframe to be appended to that number of rows. Initializes all feature
@@ -60,15 +148,12 @@ class AbSumAugmentor(object):
         oversampling to reach needed amount for given feature. If multiproc is set, calls
         to process_abstract_summary are stored in a tasks array, which is then passed to a
         function that allows multiprocessing of said summarizations to vastly reduce runtime.
-        :param df: Main dataframe
-        :param num_samples: Number of samples to pull
-        :param multiproc: Whether to run in multiprocessing mode to greatly reduce runtime
         :return: Dataframe appended with augmented samples to make underrepresented
         features match the count of the majority features.
         """
-        counts = self.get_append_counts(df)
+        counts = self.get_append_counts(self.df)
         # Create append dataframe with length of all rows to be appended
-        self.df_append = pd.DataFrame(index=np.arange(sum(counts.values())), columns=df.columns)
+        self.df_append = pd.DataFrame(index=np.arange(sum(counts.values())), columns=self.df.columns)
 
         # Creating array of tasks for multiprocessing
         tasks = []
@@ -78,38 +163,36 @@ class AbSumAugmentor(object):
             self.df_append[feature] = 0
 
         for feature in self.features:
-            num_to_append = counts[feature]
+            num_to_append = 3#counts[feature]
             for num in range(self.append_index, self.append_index + num_to_append):
-                if multiproc:
-                    tasks.append(self.process_abstract_summary(df, feature, num, num_samples))
+                if self.multiproc:
+                    tasks.append(self.process_abstract_summary(feature, num))
                 else:
-                    self.process_abstract_summary(df, feature, num, num_samples)
+                    self.process_abstract_summary(feature, num)
 
             # Updating index for insertion into shared appended dataframe to preserve indexing
             # in multiprocessing situation
             self.append_index += num_to_append
 
-        if multiproc:
+        if self.multiproc:
             run_cpu_tasks_in_parallel(tasks)
 
         return self.df_append
 
-    def process_abstract_summary(self, df, feature, num, num_samples):
+    def process_abstract_summary(self, feature, num):
         """
         Samples a subset of rows from main dataframe where given feature is exclusive. The
         subset is then concatenated to form a single string and passed to an abstract summarizer
         to generate a new data entry for the append count, augmenting said dataframe with rows
         to essentially oversample underrepresented data. df_append is set as a class variable to
         accommodate that said dataframe may need to be shared among multiple processes.
-        :param df: Main dataframe containing text and features
         :param feature: Feature to filter on
         :param num: Count of place in abs_sum_augment loop
-        :param num_samples: Number of samples to pull from main dataframe
         """
         # Pulling rows where only specified feature is set to 1
-        df_feature = df[(df[feature] == 1) & (df[self.args.features].sum(axis=1) == 1)]
-        df_sample = df_feature.sample(num_samples, replace=True)
-        text_to_summarize = ' '.join(df_sample[:num_samples]['review_text'])
+        df_feature = self.df[(self.df[feature] == 1) & (self.df[self.features].sum(axis=1) == 1)]
+        df_sample = df_feature.sample(self.num_samples, replace=True)
+        text_to_summarize = ' '.join(df_sample[:self.num_samples]['review_text'])
         new_review = self.get_abstract_summary(text_to_summarize)
         self.df_append.at[num, 'review_text'] = new_review
         self.df_append.at[num, feature] = 1
@@ -135,10 +218,10 @@ class AbSumAugmentor(object):
         feature_counts = self.get_feature_counts(df)
 
         for feature in self.features:
-            if feature_counts[feature] >= self.args.threshold:
+            if feature_counts[feature] >= self.threshold:
                 count = 0
             else:
-                count = self.args.threshold - feature_counts[feature]
+                count = self.threshold - feature_counts[feature]
 
             append_counts[feature] = count
 
@@ -159,152 +242,12 @@ def run_cpu_tasks_in_parallel(tasks):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--csv",
-        default='./data/google_pp_category.csv',
-        type=str,
-        required=True,
-        help="CSV containing data to augment.",
-    )
-
-    parser.add_argument(
-        "--aug_csv",
-        default=None,
-        type=str,
-        required=False,
-        help="Name of CSV to be saved, containing augmented data.",
-    )
-
-    parser.add_argument(
-        "--text",
-        default='text',
-        type=str,
-        required=False,
-        help="Column containing text.",
-    )
-
-    parser.add_argument(
-        "--features",
-        default=None,
-        type=str,
-        required=False,
-        help="Comma separated string of features.",
-    )
-
-    parser.add_argument(
-        "--threshold",
-        default=3500,
-        type=int,
-        required=False,
-        help="Maximum ceiling for each feature, normally the undersample max.",
-    )
-
-    parser.add_argument(
-        "--multiproc",
-        default=True,
-        type=bool,
-        required=False,
-        help="Whether to run abstract summarizations in multiprocessing mode to vasly reduce runtime.",
-    )
-
-    parser.add_argument(
-        "--debug",
-        default=True,
-        type=bool,
-        required=False,
-        help="Whether to execute print statements to review summary texts.",
-    )
-
-    parser.add_argument(
-        "--model",
-        default='t5-small',
-        type=str,
-        required=False,
-        help="Base model for T5 model and tokenizer",
-    )
-
-    parser.add_argument(
-        "--return_tensors",
-        default='pt',
-        type=str,
-        required=False,
-        help="Can be set to 'tf', 'pt' or 'np' to return respectively TensorFlow tf.constant, PyTorch torch.Tensor "
-             "or Numpy :oj: np.ndarray instead of a list of python integers.",
-    )
-
-    parser.add_argument(
-        "--num_beams",
-        default=4,
-        type=int,
-        required=False,
-        help="Number of beams for beam search. Must be between 1 and infinity. 1 means no beam search. Default to 1.",
-    )
-
-    parser.add_argument(
-        "--no_repeat_ngram_size",
-        default=4,
-        type=int,
-        required=False,
-        help="If set to int > 0, all ngrams of size no_repeat_ngram_size can only occur once.",
-    )
-
-    parser.add_argument(
-        "--min_length",
-        default=10,
-        type=int,
-        required=False,
-        help="The min length of the sequence to be generated. Between 0 and infinity. Default to 10.",
-    )
-
-    parser.add_argument(
-        "--max_length",
-        default=30,
-        type=int,
-        required=False,
-        help="The max length of the sequence to be generated. Between min_length and infinity. Default to 30.",
-    )
-
-    parser.add_argument(
-        "--early_stopping",
-        default=True,
-        type=bool,
-        required=False,
-        help="bool if set to True beam search is stopped when at least num_beams sentences finished per batch. "
-             "Defaults to False as defined in configuration_utils.PretrainedConfig.",
-    )
-
-    parser.add_argument(
-        "--skip_special_tokens",
-        default=True,
-        type=bool,
-        required=False,
-        help="Don't decode special tokens (self.all_special_tokens). Default: False.",
-    )
-
-    cl_args = parser.parse_args()
-    args = Box(vars(cl_args))
-
-    csv = cl_args.csv
-
-    aug_csv = cl_args.aug_csv
-    if not aug_csv:
-        aug_csv = csv.replace('.csv', '-augmented.csv')
-        args.aug_csv = aug_csv
-
-    df_ = pd.read_csv(csv)
-
-    # If features passed in, convert to list of strings. Otherwise assume all features aside from text are in play
-    if cl_args.features:
-        args.features = cl_args.features.split(",")
-    else:
-        args.features = df_.columns.tolist()
-        args.features.remove(args.text)
-
-    augmentor = AbSumAugmentor(args)
-    df_augmented = augmentor.abs_sum_augment(df_)
-    df_augmented.to_csv(aug_csv)
+    # Sample usage
+    csv = 'path_to_sample_csv'
+    df = pd.read_csv(csv)
+    augmentor = AbSumAugmentor(df, text='text')
+    df_augmented = augmentor.abs_sum_augment()
+    df_augmented.to_csv(csv.replace('.csv', '-augmented.csv'), encoding='utf-8', index=False)
 
 
 if __name__ == "__main__":
