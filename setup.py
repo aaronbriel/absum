09bf67c996ea9f830c0f67117d03f8cea83a53be
Aaron Briel <aaronbriel@gmail.com>
2020-10-12 07:13:24 -0500

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.6'
+VERSION = '0.2.1'
 PACKAGE_NAME = 'absum'
 AUTHOR = 'Aaron Briel'
 AUTHOR_EMAIL = 'aaronbriel@gmail.com'
